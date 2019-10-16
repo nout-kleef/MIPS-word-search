990929851cf4e4ae16bd4e0dfcb893278f644582
Nout Kleef <s1861382@frantz.inf.ed.ac.uk>
2019-10-17 00:34:38 +0100

    basically copies existing 2dstrfind implementations to wraparound files

--- inf2c-cs-cw1/wraparound.c
+++ inf2c-cs-cw1/wraparound.c
@@ -54,12 +54,136 @@ char dictionary[MAX_DICTIONARY_WORDS * (MAX_WORD_SIZE + 1 /* for \n */) + 1 /* f
 /////////////// Do not modify anything above
 ///////////////Put your global variables/functions here///////////////////////
 
+// starting indices for all dictionary entries
+int dictionary_idx[MAX_DICTIONARY_WORDS];
+// actual number of words in the dictionary
+int dict_num_words = 0;
+int grid_num_rows = 0;
+int grid_num_cols = 0;
+
+void print_word(char *word)
+{
+  while (*word != '\n' && *word != '\0')
+  {
+    print_char(*word);
+    word++;
+  }
+}
+
+void print_match(int row, int col, char *word, char direction)
+{
+  print_int(row);
+  print_char(',');
+  print_int(col);
+  print_char(' ');
+  print_char(direction);
+  print_char(' ');
+  print_word(word);
+  print_char('\n');
+}
+
+int contain_hor(char *string, char *word)
+{
+  while (1)
+  {
+    if (*string != *word)
+      return *word == '\n';
+    string++; // 1 right, 0 down
+    word++;
+    // after this increment, we may have run into the following:
+    //      X   X   \n
+    //     [h] [i] [\n]
+    //      X   X   \n
+    // and if our word is "hi\n", we'll still have a match.
+    if (*string == '\n')
+      return *word == '\n';
+  }
+}
+
+int contain_ver(char *string, char *word)
+{
+  while (1)
+  {
+    if (*string != *word)
+      return *word == '\n';
+    string += grid_num_cols + 1; // 0 right, 1 down
+    word++;
+  }
+}
+
+int contain_dia(char *string, char *word)
+{
+  while (1)
+  {
+    if (*string != *word)
+      return *word == '\n';
+    string += grid_num_cols + 2; // 1 right, 1 down
+    word++;
+    // after this increment, we may have run into the following:
+    //     [h] X  \n
+    //      X [i] \n
+    //      X  X [\n]
+    // and if our word is "hi\n", we'll still have a match.
+    if (*string == '\n')
+      return *word == '\n';
+  }
+}
+
+void strfind()
+{
+  int wordfound = 0;
+  int idx = 0;
+  int grid_idx = 0;
+  int xcoord = 0;
+  int ycoord = 0;
+  char *word;
+  // for each row
+  while (grid[grid_idx] != '\0')
+  {
+    while (grid[grid_idx] != '\n')
+    {
+      // no need to check for '\0', grid files are terminated by '\n',
+      // so we will definitely break out of this loop.
+      for (idx = 0; idx < dict_num_words; idx++)
+      {
+        word = dictionary + dictionary_idx[idx]; // point to dict entry
+        if (contain_hor(grid + grid_idx, word))
+        {
+          print_match(ycoord, xcoord, word, 'H');
+          wordfound = 1; // flag that we've found a word
+        }
+        if (contain_ver(grid + grid_idx, word))
+        {
+          print_match(ycoord, xcoord, word, 'V');
+          wordfound = 1; // flag that we've found a word
+        }
+        if (contain_dia(grid + grid_idx, word))
+        {
+          print_match(ycoord, xcoord, word, 'D');
+          wordfound = 1; // flag that we've found a word
+        }
+      }
+      grid_idx++;
+      xcoord++;
+    }
+    grid_idx++; // skip over '\n'
+    xcoord = 0;
+    ycoord++;
+  }
+  if (wordfound)
+    return; // skip over printing "-1"
+  print_string("-1\n");
+}
+
 //---------------------------------------------------------------------------
 // MAIN function
 //---------------------------------------------------------------------------
 
 int main(void)
 {
+
+  int dict_idx = 0;
+  int start_idx = 0;
 
   /////////////Reading dictionary and grid files//////////////
   ///////////////Please DO NOT touch this part/////////////////
@@ -121,6 +245,41 @@ int main(void)
   fclose(dictionary_file);
   //////////////////////////End of reading////////////////////////
   ///////////////You can add your code here!//////////////////////
+
+  // compute the actual dimensions of the grid
+  char *c = grid;
+  grid_num_rows = 0;
+  while (*c != '\0')
+  {
+    grid_num_cols = 0;
+    grid_num_rows++;
+    while (*c != '\n')
+    {
+      c++;
+      grid_num_cols++;
+    }
+    c++; // pass over '\n'
+  }
+
+  idx = 0;
+  do
+  {
+    c_input = dictionary[idx];
+    if (c_input == '\0')
+    {
+      break;
+    }
+    if (c_input == '\n')
+    {
+      dictionary_idx[dict_idx++] = start_idx;
+      start_idx = idx + 1;
+    }
+    idx += 1;
+  } while (1);
+
+  dict_num_words = dict_idx;
+
+  strfind();
 
   return 0;
 }
